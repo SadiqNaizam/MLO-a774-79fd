0c1d19202cb33d6b6597d34bade9dcb8f322315b
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-11 20:07:29 +0530

    Updated code files: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,39 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-// Import newly created pages
+// Import pages
 import AccountsDashboardPage from "./pages/AccountsDashboardPage";
 import FundTransferPage from "./pages/FundTransferPage";
 import CardControlsPage from "./pages/CardControlsPage";
 import TransactionDetailsModalPage from "./pages/TransactionDetailsModal";
 
-import NotFound from "./pages/NotFound"; // Always Must Include (Assuming we already have NotFound.tsx)
+import NotFound from "./pages/NotFound"; 
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
+    <TooltipProvider delayDuration={300}> {/* Adjusted delay duration slightly */}
       <Toaster />
-      <Sonner />
+      <Sonner richColors /> {/* Enable rich colors for Sonner */}
       <BrowserRouter>
         <Routes>
-          {/* Assuming AccountsDashboardPage is the main page after login */}
-          <Route path="/" element={<AccountsDashboardPage />} /> 
+          {/* Default route to AccountsDashboardPage */}
+          <Route path="/" element={<Navigate to="/accounts-dashboard" replace />} /> 
+          
           <Route path="/accounts-dashboard" element={<AccountsDashboardPage />} />
           <Route path="/fund-transfer" element={<FundTransferPage />} />
           <Route path="/card-controls" element={<CardControlsPage />} />
-          <Route path="/transaction-details" element={<TransactionDetailsModalPage />} /> 
-          {/* Example of how transaction details might take an ID, though the page currently uses a static/state-passed ID */}
-          {/* <Route path="/transaction-details/:transactionId" element={<TransactionDetailsModalPage />} /> */}
+          <Route path="/transaction-details" element={<TransactionDetailsModalPage />} />
+          
+          {/* Placeholder routes for BottomNavigation example links, point them to relevant pages or NotFound */}
+          <Route path="/accounts-overview" element={<AccountsDashboardPage />} /> {/* Example */}
+          <Route path="/insights" element={<NotFound />} /> {/* Example */}
+          <Route path="/card-services" element={<CardControlsPage />} /> {/* Example */}
+          <Route path="/more" element={<NotFound />} /> {/* Example */}
+
 
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
            <Route path="*" element={<NotFound />} /> {/* Always Include This Line As It Is. */}
